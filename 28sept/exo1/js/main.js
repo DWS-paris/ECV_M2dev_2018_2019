4101bda7f586cbc92aa8c4abdbd6a7fb563eccbb
DWS-paris <j.noyer@digitalworkshop.fr>
2018-09-28 09:57:52 +0200

    Configuration de PouchDB et CouchDB

--- 28sept/exo1/js/main.js
+++ 28sept/exo1/js/main.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+    /*
+    Configuration PouchDB / CouchDB
+    */
+        // Définition de la base de données dans le navigateur
+        const innerDB = new PouchDB('todo');
 
+        // Définition de la base de données externe
+        const remoteDB = 'https://ldp.dwsapp.io/my-todo';
+
+        // Afficher les informations sur la base interne
+        console.log( innerDB.info() );
+
+        // Création d'une fonction de synchronisation PouchDB <=> CouchDB
+        function syncDBS(){
+            innerDB.replicate.to( remote, { live: true }, console.log('Nothing to SYNC') );
+            innerDB.replicate.from( remote, { live: true }, console.log('Nothing to SYNC') );
+        };
+    //
+
+    /*
+    Interface utilisateur
+    */
+    //
 });
